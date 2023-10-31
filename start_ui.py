df5ba55e8c393ccf9c898d5e95f2eed0077646e9
Lettera <lorenzocav97@gmail.com>
2023-10-31 19:27:29 +0100

    fixed scan db button

--- start_ui.py
+++ start_ui.py
@@ -1,9 +1,7 @@
 import gradio as gr
 
-from constants.constant import PROPERTIES_FILE_JSON_NAME
 from manager.file_manager import scan_db
-from manager.properties_manager import write_property
-from manager.ui_manager import search_user_db, search_sentence_db
+from manager.ui_manager import search_sentence_db, search_user_db
 
 
 def ui_function_search_user(user_input):
@@ -12,6 +10,10 @@ def ui_function_search_user(user_input):
 
 def ui_function_search_sentence(user_input):
     return search_sentence_db(user_input)
+
+
+def refresh_dbs():
+    return gr.Dropdown(choices=scan_db(), value=scan_db()[0])
 
 
 with gr.Blocks() as demo:
@@ -25,8 +27,15 @@ with gr.Blocks() as demo:
                 outputs=[dropdown],
             )
             scan_db_text_button = gr.Button("🔄 Scan For DB")
-            scan_db_text_button.click(write_property(property_key=PROPERTIES_FILE_JSON_NAME,
-                                                     property_value=dropdown), [], [])
+            # scan_db_text_button.click(
+            #     write_property(
+            #         property_key=PROPERTIES_FILE_JSON_NAME, property_value=dropdown
+            #     ),
+            #     [],
+            #     [],
+            # )
+            scan_db_text_button.click(refresh_dbs, [], [dropdown])
+
         with gr.Row() as btn_row:
             create_db_text_button = gr.Button("Create a DB")
             select_db_text_button = gr.Button("Select DB")
@@ -44,9 +53,14 @@ with gr.Blocks() as demo:
             sentence_text_output = gr.TextArea()
             sentence_text_button = gr.Button("Search Sentence")
 
-        user_text_button.click(ui_function_search_user, inputs=user_text_input, outputs=user_text_output)
-        sentence_text_button.click(ui_function_search_sentence, inputs=sentence_text_input,
-                                   outputs=sentence_text_output)
+        user_text_button.click(
+            ui_function_search_user, inputs=user_text_input, outputs=user_text_output
+        )
+        sentence_text_button.click(
+            ui_function_search_sentence,
+            inputs=sentence_text_input,
+            outputs=sentence_text_output,
+        )
 
 
 demo.launch()
